9e46114097cbfcbeb49b8eea49e6d8ddce9de16f
Aditya Puri <91184725+adityapurigithub@users.noreply.github.com>
2026-03-18 23:01:14 +0530

    Update TicTacToe.jsx

--- TicTacToe.jsx
+++ TicTacToe.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const TicTacToe = () => {
-  const [player, setPlayer] = React.useState("X");
-  const [squares, setSquares] = React.useState(Array(9).fill(null)); //empty array of 9 squares
+  const [player, setPlayer] = useState("X");
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   let winPattern = [
     [0, 1, 2], // row
@@ -15,59 +15,50 @@ const TicTacToe = () => {
     [2, 4, 6],
   ];
 
-  const isWon = () => {
+  const handleReset = () => {
+    setSquares(Array(9).fill(null));
+    setPlayer("X");
+  };
+  const checkWin = () => {
     for (let i = 0; i < winPattern.length; i++) {
       const [a, b, c] = winPattern[i];
+
       if (
         squares[a] &&
         squares[a] === squares[b] &&
-        squares[a] === squares[c]
+        squares[b] === squares[c]
       ) {
-        return alert(`Won :${squares[a]}`);
+        console.log(winPattern[i]);
+
+        alert(`${squares[a]} wins`);
+        handleReset();
       }
     }
-    return null;
   };
 
   const handleSqClick = (id) => {
-    if (isWon()) {
-      return;
-    }
+    if (squares[id]) return; //cliced same square
 
-    if (squares[id]) return; //if the square is already clicked, return
-
-    const updatedSq = [...squares];
-    updatedSq[id] = player;
-
-    setSquares(updatedSq);
-
+    const squaresCopy = [...squares];
+    squaresCopy[id] = player;
+    setSquares(squaresCopy); //update the array position with current player
     setPlayer(player === "X" ? "O" : "X");
-  };
 
-  const handleReset = () => {
-    setPlayer("X");
-    setSquares(Array(9).fill(null));
+    checkWin();
   };
 
   return (
-    <div className="flex flex-col gap-2 items-center justify-center">
-      <div className="border grid grid-cols-3 my-2">
-        {Array.from({ length: 9 }).map((_, index) => (
+    <div className="flex items-center justify-center my-4">
+      <div className="grid grid-cols-3">
+        {Array.from({ length: 9 }, (_, index) => (
           <div
-            className="border p-4 h-16 w-16 flex alich-center justify-center"
-            key={index}
+            className="p-5 px-6 border h-16 w-16"
             onClick={() => handleSqClick(index)}
           >
             {squares[index]}
           </div>
         ))}
       </div>
-      <button
-        className="p-2 px-4 rounded bg-black text-white dark:bg-white dark:text-black"
-        onClick={handleReset}
-      >
-        Reset
-      </button>
     </div>
   );
 };
